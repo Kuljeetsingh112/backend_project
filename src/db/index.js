89089fee947002d6201d0c98fef7e89c6686657c
Kuljeetsingh112 <kuljeetkhalsa112@gmail.com>
2024-02-02 12:31:37 +0530

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js"
 const connectDB = async () => {
     try {
         const Connected = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`mongodb connected successfully Host: ${Connected.connection.host}`)
+        console.log(`mongodb connected successfully!! DB Host: ${Connected.connection.host}`)
     } catch (error) {
         console.log("ERROR: ", error)
         process.exit(1)
